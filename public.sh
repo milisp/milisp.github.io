d04666cb7969fd930a13bf209a1942ebb4b29dcb
milisp <milisp@proton.me>
2024-09-27 10:52:07 +0800

    A: public script

---
+++ public.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "ensure in branch content[y/n]: "
+
+read yn
+if [ "$yn" = "y" ]; then
+    echo "input commit message: "
+    read commit_msg
+    git checkout content
+    git add content/posts/
+    git commit -m "$commit_msg"
+    command mv public/* ../
+    git push origin content
+
+    git checkout main
+    git commit -m "A: add post"
+fi
